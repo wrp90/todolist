a2768f6b742029aef233622aa80b205db9731b1d
Will Papp <wpapp90@gmail.com>
2023-01-21 11:15:37 -0600

    added text bar

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const root = document.getElementById('root');
 
 const headerElement = document.createElement('h1');
+const taskBar = document.createElement('input');
 
 headerElement.innerText = 'Hello'
 
 root.append(headerElement)
+root.append(taskBar)
