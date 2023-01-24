6cfee2654cbacb330ec3324c2d3472910f153cff
Will Papp <wpapp90@gmail.com>
2023-01-24 15:08:22 -0600

    added textbar with button

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const root = document.getElementById('root');
 
 const headerElement = document.createElement('h1');
-const taskBar = document.createElement('input');
 
 headerElement.innerText = 'Hello'
 
 root.append(headerElement)
-root.append(taskBar)
+
