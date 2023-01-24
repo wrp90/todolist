c0ea8a305c2a9560a00674da6832c94f4b318662
Will Papp <wpapp90@gmail.com>
2023-01-24 15:37:31 -0600

    fixed list, can now remove items from list

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const addItem = () => {
-    let list = document.getElementById('list');
+    let list = document.getElementById('dynamicList');
     let textBox = document.getElementById('textBox');
     let item = document.createElement('li');
-    list.setAttribute('id', textBox.value);
-    list.appendChild(document.createTextNode(textBox.value));
-    item.appendChild(item);
+    item.setAttribute('id', textBox.value);
+    item.appendChild(document.createTextNode(textBox.value));
+    list.appendChild(item);
 }
 
-
+const removeItem = () => {
+    let list = document.getElementById('dynamicList');
+    let textBox = document.getElementById('textBox');
+    let item = document.getElementById(textBox.value);
+    list.removeChild(item);
+}
